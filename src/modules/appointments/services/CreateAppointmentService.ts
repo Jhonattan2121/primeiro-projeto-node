0a820df8001393107062f6362c37c7973fe7e6fe
jhonattan2121 <jhoontutors@gmail.com>
2023-01-07 21:28:08 -0300

    reescrevendo repositorio

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -4,47 +4,31 @@ import { startOfHour } from "date-fns";
 import { getCustomRepository } from "typeorm";
 import AppError from "@shared/errors/AppError";
 
-/*
-* [x] recebimento de informacoes
-* [x] tratativa de erros/excessoes
-* [x] acesso ao repositorio
-
-*/ 
-
-interface Request { // DTO data transfer object
+interface Request { 
   provider_id: string;
   date: Date;
 }
 
-
-/*
-* dependency inversion (SOLID) (inversão de dependência)
-* single responsability principle (princípio da responsabilidade única)
-*/ 
- //DRY: dont repeat yourself
-
-class CreateAppointmentService { //executando a criancao de um novo appointment
+class CreateAppointmentService { 
     public async execute({ date , provider_id }: Request): Promise <Appointment> {
       const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
-      const appointmentDate = startOfHour(date); // regra de negocio , agendamento de hora em hora
+      const appointmentDate = startOfHour(date); 
 
-      const findAppointmentInSameDate = await appointmentsRepository.findByDate(appointmentDate); // retornar um objeto appointmnet se tiver um agendamento pela data e um unico parametro data
+      const findAppointmentInSameDate = await appointmentsRepository.findByDate(appointmentDate);
 
-    if (findAppointmentInSameDate) { //encontrando um appointment na mesma data
-      throw new AppError('this appointment is already booked'); //lancando um erro
+    if (findAppointmentInSameDate) {
+      throw new AppError('this appointment is already booked'); 
     
     } 
 
-    const appointment = appointmentsRepository.create({ 
+    const appointment = await appointmentsRepository.create({ 
     provider_id,
     date: appointmentDate,
   }); 
-    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
 }
 export default CreateAppointmentService;
 
-//service nao tem acesso direto aos dados da requisicao e os dados da resposta

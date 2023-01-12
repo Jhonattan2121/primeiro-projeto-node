7d70a3a064560db983d9c731a86ec2857e903d72
jhonattan2121 <jhoontutors@gmail.com>
2023-01-12 20:54:47 -0300

    Testando criação de usuário

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,36 @@
+
+import AppError from "@shared/errors/AppError";
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import CreateUserService from './CreateUserService';
+describe('CreateUser', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+    );
+    const user = await createUser.execute({
+        name: 'Jhon Ferri',
+        email:  'jhonferri@gmail.com',
+        password: '123456789',
+      });
+      expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with same email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+    );
+    const user = await createUser.execute({
+        name: 'Jhon Ferri',
+        email:  'jhonferri@gmail.com',
+        password: '123456789',
+      });
+      expect(createUser.execute({
+        name: 'Jhon Ferri',
+        email:  'jhonferri@gmail.com',
+        password: '123456789',
+      }),
+       ).rejects.toBeInstanceOf(AppError);
+  });
+});

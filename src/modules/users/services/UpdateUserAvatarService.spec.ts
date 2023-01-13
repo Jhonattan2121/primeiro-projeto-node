aee2703d04aa0e40e0e27d8d3af0f1fddf3bbcd0
jhonattan2121 <jhoontutors@gmail.com>
2023-01-13 04:32:59 -0300

    Atualização de Avatar

--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -66,12 +66,11 @@ describe('UpdateUserAvatar', () => {
 
       await updateUserAvatar.execute({
         user_id: user.id,
-        avatarFilename: 'avatarTwo.jpg',
+        avatarFilename: 'avatar2.jpg',
       });
       
       expect(deleteFile).toHaveBeenCalledWith('avatar.jpg');
 
-      expect(user.avatar).toBe('avatar.jpg');
+      expect(user.avatar).toBe('avatar2.jpg');
   });
-
 });
